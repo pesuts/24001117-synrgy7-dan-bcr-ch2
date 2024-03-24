59c5881b2f04b70d45bcc1faae3b592863a6376e
danielsinaga <daniel.sinaga.ds@gmail.com>
2024-03-25 04:21:52 +0700

    chore: initial commit challenge 1

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      spacing: {
+        13: 2,
+      }
+    },
+  },
+  plugins: [],
+}
