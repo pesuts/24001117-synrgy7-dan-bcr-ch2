1709ae8b26ad5ce00ba033ee176f03478ba24656
danielsinaga <daniel.sinaga.ds@gmail.com>
2024-03-26 02:45:28 +0700

    feat: add responsive layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,15 @@ module.exports = {
     extend: {
       spacing: {
         13: 2,
-      }
+      },
+      colors: {
+        'light-blue': 'rgba(241, 243, 255, 1)',
+        'lime-green': 'rgba(92, 184, 95, 1)',
+        'lime-green-hover': 'rgb(45, 90, 47)',
+      },
+      content: {
+        'checklist': 'url("img/png/check.png")',
+      },
     },
   },
-  plugins: [],
 }
