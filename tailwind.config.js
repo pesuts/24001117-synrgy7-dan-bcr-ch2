22fbf41a60194b9ff367e0331168c89051a6f8b9
danielsinaga <daniel.sinaga.ds@gmail.com>
2024-03-30 06:47:34 +0700

    feat: add responsive layout for desktop and fix carousel

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
         'light-blue': 'rgba(241, 243, 255, 1)',
         'lime-green': 'rgba(92, 184, 95, 1)',
         'lime-green-hover': 'rgb(45, 90, 47)',
+        'blue': 'rgba(13, 40, 166, 1)',
       },
       content: {
         'checklist': 'url("img/png/check.png")',
